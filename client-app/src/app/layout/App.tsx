bd67a27b0657218ca6e40b06171130ed238f037d
Ertan AKTAŞ <aktasertan11@gmail.com>
2022-10-09 03:22:02 +0300

    module 5

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,80 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container} from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../feature/activities/dashboard/ActivityDashboard';
+import uuid from 'react-uuid';
+
+
+function App() {
+  const[activities, setActivities] = useState<Activity[]>([]);
+  const[selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined)
+  const[editMode, setEditMode] = useState(false)
+
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
+      setActivities(response.data);
+    })
+  }, []);
+
+  function handleSelectActivity(id : string){
+    setSelectedActivity(activities.find(x => x.id === id))
+    
+  }
+  function handleCancelSelectActivity(){
+
+    setSelectedActivity(undefined);
+  }
+  function handleFormOpen(id?:string){
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+  function handleFormClose(){
+    setEditMode(false);
+  }
+
+  function handleCreateOrEditActivity(activity: Activity){
+
+    activity.id ? setActivities([...activities.filter( x => x.id !== activity.id), activity])
+                : setActivities([...activities, {...activity, id: uuid()}])
+
+                setEditMode(false);
+                setSelectedActivity(activity);
+  }
+  function handleDeleteActivity(id:string){
+      setActivities([...activities.filter(x => x.id !==id)])
+      setSelectedActivity(undefined);
+  }
+
+
+
+  return (
+    <Fragment>
+      <NavBar openForm={handleFormOpen}/>
+      <Container style={{marginTop: '5em'}}>
+      <ActivityDashboard 
+      activities={activities} 
+      selectedActivity={selectedActivity}
+      selectActivity={handleSelectActivity}
+      cancelSelectActivity={handleCancelSelectActivity}
+      editMode={editMode}
+      openForm={handleFormOpen}
+      closeForm={handleFormClose}
+      createOrEdit={handleCreateOrEditActivity}
+      deleteActivity = {handleDeleteActivity}
+
+
+
+      
+      />
+      </Container>
+      
+        </Fragment>
+       
+      
+    
+  );
+}
+
+export default App;
